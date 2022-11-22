53d0a24b52d0c7c4a8b1a8f797b1e77d8791b540
Nic Strong <nic.strong@ipfx.com>
2022-11-22 18:45:16 +1300

    Added ability to record and save the video too a file

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useRef } from 'react'
+import React, { useEffect, useRef, useState } from 'react'
 import { Results, SelfieSegmentation } from "@mediapipe/selfie_segmentation"
 import './App.css'
 import { Camera } from '@mediapipe/camera_utils'
@@ -8,6 +8,11 @@ function App() {
   const outputVideoRef = useRef<HTMLVideoElement>(null)
   const canvasRef = useRef<HTMLCanvasElement>(null)
   const contextRef = useRef<CanvasRenderingContext2D | null>(null)
+  const canvasStreamRef = useRef<MediaStream | null>(null)
+  const mediaRecorderRef = useRef<MediaRecorder | null>(null)
+  const recordedDataRef = useRef<Blob[] | null>(null)
+  const [recording, setRecording] = useState(false)
+  const [hasRecorededData, setHasRecorededData] = useState(false)
 
   useEffect(() => {
     if (!canvasRef.current) return;
@@ -17,8 +22,9 @@ function App() {
 
     // render the canvas too a video element to play, this way
     // we can also wrap the stream in a MediaRecorder to do what we want with the video
-    const stream = canvasRef.current.captureStream()
+    const stream = canvasRef.current.captureStream(30)
     outputVideoRef.current.srcObject = stream
+    canvasStreamRef.current = stream
 
     const selfieSegmentation = new SelfieSegmentation({
       locateFile: (file) =>
@@ -34,7 +40,7 @@ function App() {
     const camera = new Camera(inputVideoRef.current, {
       onFrame: async () => {
         if (inputVideoRef.current) {
-          await selfieSegmentation.send({image: inputVideoRef.current});
+          await selfieSegmentation.send({ image: inputVideoRef.current });
         }
       },
       width: 1280,
@@ -43,6 +49,59 @@ function App() {
     camera.start();
 
   }, [])
+
+  const handleStart = () => {
+    if (!canvasStreamRef.current) return
+    setRecording(true)
+    recordedDataRef.current = []
+    setHasRecorededData(false)
+    mediaRecorderRef.current = new MediaRecorder(canvasStreamRef.current, { mimeType: 'video/webm;codecs=vp9' })
+
+    mediaRecorderRef.current.ondataavailable = e => {
+      if (recordedDataRef.current === null) return // we already stopped
+      recordedDataRef.current.push(e.data)
+    }
+
+    mediaRecorderRef.current.onstop = async () => {
+      setHasRecorededData(true)
+      mediaRecorderRef.current = null
+    }
+    mediaRecorderRef.current.start(200)
+  }
+
+  const handleStop = () => {
+    try {
+      if (mediaRecorderRef.current && mediaRecorderRef.current.state === 'recording') {
+        mediaRecorderRef.current.stream.getTracks().forEach(track => track.stop())
+        mediaRecorderRef.current.stop()
+      }
+    } catch (e) {
+      console.log('handleStop: error', e)
+    }
+  }
+
+  const handleSave = () => {
+    let sourceBlob: Blob | null = null
+    try {
+      if (recordedDataRef.current === null) return // we already stopped
+
+      sourceBlob = new Blob(recordedDataRef.current, {
+        type: 'video/webm',
+      })
+      recordedDataRef.current = null
+    } catch (e) {
+      console.log('mediaRecorder.onStop error:', e)
+      return
+    }
+
+    const a = document.createElement('a')
+    a.style.cssText = 'display: none'
+    const url = window.URL.createObjectURL(sourceBlob)
+    a.href = url
+    a.download = 'test.webm'
+    a.click()
+    window.URL.revokeObjectURL(url)
+  }
 
   const onResults = (results: Results) => {
     if (!contextRef.current || !canvasRef.current) return
@@ -63,6 +122,12 @@ function App() {
       <video className='videoSource' autoPlay ref={inputVideoRef} />
       <canvas className='canvas' ref={canvasRef} width={1280} height={720} />
       <video className='videoDest' autoPlay ref={outputVideoRef} />
+
+      <div className='controls'>
+        <button disabled={recording} onClick={() => handleStart()}>Start</button>
+        <button disabled={!recording} onClick={() => handleStop()}>Stop</button>
+        <button disabled={!hasRecorededData} onClick={() => handleSave()}>Save</button>
+      </div>
     </div>
   )
 }
